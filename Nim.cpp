2d6142ff4f738c79e9c24e5a0ad35de7c4a8e5c2
OwenMt50 <Omartin005@student.butte.edu>
2025-04-21 05:35:49 +0000

    commit

--- Nim.cpp
+++ Nim.cpp
@@ -7,7 +7,7 @@ int main(){
     int sticks = 16;
     int remove = 0;
     int turn = 0;
-    int computer = rand() % 4;
+    int computer = rand() % 3;
     string name = "";
     
     cout<<"Please input a creative username\n";
@@ -19,14 +19,14 @@ int main(){
     while (sticks > 0){
         cout<<"Please remove a number of sticks from 1 to 3.\n";
         cin>>remove;
-        sticks = (sticks - remove) - computer;
+        sticks = sticks - remove;
+        sticks = sticks - computer;
         cout<<"There are now "<<sticks<<" sticks left. \n";
     }
 
-    if (sticks = 0){
-        cout<<"Computer opponent has won the round.\n";
-    }else{
+    if (sticks < 0){
         cout<<"Congratulations, "<<name<<", you have bested the Computer!\n";
+    }else{ if(sticks = 0);
+        cout<<"Computer opponent has won the round.\n";
     }
-    return 0;
 }
